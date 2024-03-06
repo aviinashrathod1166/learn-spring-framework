3fb9242d3b86d8da97829bad94b528514df56187
Avinash <aviinashrathod1166@gmail.com>
2024-03-06 15:02:59 +0530

    contrller test commit

--- src/main/java/com/in28minutes/learnspringframework/SpringController.java
+++ src/main/java/com/in28minutes/learnspringframework/SpringController.java
@@ -5,7 +5,7 @@ package com.in28minutes.learnspringframework;
 public class SpringController {
 	
 	public String ReadController() {
-		return "I am in Contrller......";
+		return "I am in Contrller......Avinash";
 		
 	}
 
