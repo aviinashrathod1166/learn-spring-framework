6fb056af0aa20c1a582e33305b0128175427671d
Avinash <aviinashrathod1166@gmail.com>
2024-03-08 10:07:55 +0530

    Mario game & Runner

---
+++ src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame {
+	public void up() {
+		System.out.println("jump");
+	}
+
+	public void down() {
+		System.out.println("Go into a hole");
+	}
+
+	public void left() {
+		System.out.println("Go Back");
+	}
+
+	public void Right() {
+		System.out.println("Accelerate");
+	}
+
+}
