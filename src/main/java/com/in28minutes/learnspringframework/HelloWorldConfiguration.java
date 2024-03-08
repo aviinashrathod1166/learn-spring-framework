39f9c75d6260dfccf1e15745a7f0f67c7bab3290
Avinash <aviinashrathod1166@gmail.com>
2024-03-08 13:18:30 +0530

    Created Costom bean

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {};
+record Person(String name, int age, Address address) {};
 
 record Address(String firstLine, String city) {};
 
@@ -21,12 +21,23 @@ public class HelloWorldConfiguration {
 
 	@Bean
 	public Person person() {
-		var person = new Person("Sapana", 30);
+		var person = new Person("Sapana", 30,new Address("Near Airport, Jalna Road ", "Sambhajinagar"));
 		return person;
 
 	}
-
+	
+	
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(),new Address("Pune-Banglore Highway ", "Pune"));
+	}
+	
+	@Bean
+	public Person person3Parameters(String name,int age, Address address2) {//name,age,address2
+		return new Person(name, age,address());
+	}
+
+	@Bean(name="address2")
 	public Address address() {
 		var address = new Address("Near Airport, Jalna Road ", "Sambhajinagar");
 		return address;
