557e3737547a3c0ebc5d5965b7fafe108fe727ac
Avinash <aviinashrathod1166@gmail.com>
2024-03-08 12:44:57 +0530

    Record used

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,26 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {
+};
+
 @Configuration
 public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
 		return "Avinash";
+	}
+
+	@Bean
+	public int age() {
+		return 32;
+	}
+
+	@Bean
+	public Person person() {
+		var person = new Person("Sapana", 30);
+		return person;
+
 	}
 
 }
