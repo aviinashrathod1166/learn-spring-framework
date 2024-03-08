2b89287bdea96084a5c0828661be0ebfcbe4820e
Avinash <aviinashrathod1166@gmail.com>
2024-03-08 12:51:21 +0530

    Address bean craeted

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -10,6 +10,7 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("name"));
 		System.out.println(context.getBean("age"));
 		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 	}
 
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,8 +3,9 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {
-};
+record Person(String name, int age) {};
+
+record Address(String firstLine, String city) {};
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -22,6 +23,13 @@ public class HelloWorldConfiguration {
 	public Person person() {
 		var person = new Person("Sapana", 30);
 		return person;
+
+	}
+
+	@Bean
+	public Address address() {
+		var address = new Address("Near Airport, Jalna Road ", "Sambhajinagar");
+		return address;
 
 	}
 
